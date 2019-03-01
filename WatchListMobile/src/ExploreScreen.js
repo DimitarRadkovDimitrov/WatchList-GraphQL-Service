b70ee75f4a6ff2d6699f1fdc76a58fa1afa62f22
DimitarRadkovDimitrov <ddimitrov360@gmail.com>
2019-03-01 17:29:59 -0500

    Build RecordList component as clickable image list

--- WatchListMobile/src/ExploreScreen.js
+++ WatchListMobile/src/ExploreScreen.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {View, AppRegistry, ScrollView, Image, Text} from 'react-native';
+import RecordList from './RecordList';
 
 class ExploreScreen extends React.Component 
 {
     render() 
     {
         return (
-            null
+            <View>
+                <RecordList header='Popular' queryName='popularMovies'/>
+                <RecordList header='Top Rated' queryName='topRatedMovies'/>
+                <RecordList header='Now Playing' queryName='nowPlayingMovies'/>
+                <RecordList header='Upcoming' queryName='upcomingMovies'/>  
+            </View>
         );
     }
 }
